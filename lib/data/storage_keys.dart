d19a15b109464bf8cada31ca5185ec5b9c978929
DanhDue ExOICTIF <danhdue@gmail.com>
2022-10-06 16:11:56 +0700

    add the app configuration model.

---
+++ lib/data/storage_keys.dart
@@ -0,0 +1,8 @@
+class StorageKeys {
+  static const notification = 'notification';
+
+  /// All Hive's typeIds are between 0 and 223 are allowed.
+  static const appConfigurationHiveTypeId = 0;
+
+  static const appConfigBoxName = 'appConfigBoxName';
+}
--- lib/screens/base/base_binding_creator_widget.dart
+++ lib/screens/base/base_binding_creator_widget.dart
@@ -1,9 +1,8 @@
-import 'package:d3f_core/app.dart';
 import 'package:d3f_core/binding_creator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
-import 'package:get/get_instance/src/bindings_interface.dart';
 import 'package:get/get.dart';
+import 'package:get/get_instance/src/bindings_interface.dart';
 
 import 'base_controller.dart';
 import 'base_view.dart';
